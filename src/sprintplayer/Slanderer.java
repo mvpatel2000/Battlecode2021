1f53832144d3c3241fc8ea24534b8a9ff8364dce
Mihir <mihir.v.patel7@gmail.com>
2021-01-04 23:28:39 -0500

    add broken up sprint player

---
+++ src/sprintplayer/Slanderer.java
@@ -0,0 +1,17 @@
+package sprintplayer;
+
+import battlecode.common.*;
+
+public class Slanderer extends Unit {
+
+    public Slanderer(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        super.run();
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
+    }
+}
