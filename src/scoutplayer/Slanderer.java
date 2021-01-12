9e68dbff990f98c6b73f1617d7885a2a20013658
Mihir <mihir.v.patel7@gmail.com>
2021-01-12 14:56:16 -0800

    fix slanderers

--- src/scoutplayer/Slanderer.java
+++ src/scoutplayer/Slanderer.java
@@ -30,6 +30,14 @@ public class Slanderer extends Unit {
     public void run() throws GameActionException {
         super.run();
 
+        // If you turn into politician, suicide for now
+        // TODO: Listen to spawn messages from EC to learn new dest
+        if (rc.getType() == RobotType.POLITICIAN) {
+            if (rc.canEmpower(1)) {
+                rc.empower(1);
+            }
+        }
+
         // Run away from nearest Muckraker.
         if (rc.isReady()) {
             RobotInfo nearestMuckraker = null;
@@ -47,7 +55,7 @@ public class Slanderer extends Unit {
                 int diffY = myLocation.y - nearestMuckraker.location.y;
                 destination = destination.translate(diffX, diffY);
             } 
-            fuzzyMove(destination);
+            wideFuzzyMove(destination);
         }
 
         if (!flagSetThisRound) {
