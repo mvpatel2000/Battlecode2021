2e101ffdd6d88a0d57a4f2860766d7d2978cc9f7
Vinjai Vale <vinjai.vale@gmail.com>
2021-01-17 18:48:36 -0500

    unitupdateflag test

--- test/flags/SpecificFlagTests.java
+++ test/flags/SpecificFlagTests.java
@@ -7,11 +7,12 @@ import scoutplayer.Flag;
 import scoutplayer.SpawnUnitFlag;
 import scoutplayer.LocationFlag;
 import scoutplayer.SpawnDestinationFlag;
+import scoutplayer.UnitUpdateFlag;
 
 public class SpecificFlagTests {
 
 	@Test
-	public void testNewSpawnUnitFlag() {
+	public void testSpawnUnitFlag() {
         SpawnUnitFlag suf = new SpawnUnitFlag(RobotType.POLITICIAN, Direction.EAST, 19344);
         assertEquals(Flag.SPAWN_UNIT_SCHEMA, suf.getSchema());
         assertEquals(RobotType.POLITICIAN, suf.readUnitType());
@@ -20,7 +21,7 @@ public class SpecificFlagTests {
     }
 
     @Test
-	public void testNewLocationFlag() {
+	public void testLocationFlag() {
         LocationFlag lf = new LocationFlag(new MapLocation(23847, 22947));
         assertEquals(lf.readLocation()[0], 23847 & 127);
         assertEquals(lf.readLocation()[1], 22947 & 127);
@@ -28,9 +29,15 @@ public class SpecificFlagTests {
     }
 
     @Test
-    public void testNewSpawnDestinationFlag() {
+    public void testSpawnDestinationFlag() {
         SpawnDestinationFlag sdf = new SpawnDestinationFlag(new MapLocation(23847, 22931), SpawnDestinationFlag.INSTR_SCOUT);
         assertEquals(Flag.SPAWN_DESTINATION_SCHEMA, sdf.getSchema());
         assertEquals(SpawnDestinationFlag.INSTR_SCOUT, sdf.readInstruction());
+    }
+
+    @Test
+    public void testUnitUpdateFlag() {
+        UnitUpdateFlag uuf = new UnitUpdateFlag(13409113);
+        assertEquals(false, uuf.readIsSlanderer());
     }
 }
