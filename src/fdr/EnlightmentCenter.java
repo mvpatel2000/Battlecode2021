a5e404e2064a41c30aa5c3c1eaecc8ff04831751
Mihir <mihir.v.patel7@gmail.com>
2021-01-22 10:12:01 -0800

    go faster

--- src/fdr/EnlightmentCenter.java
+++ src/fdr/EnlightmentCenter.java
@@ -132,7 +132,7 @@ public class EnlightmentCenter extends Robot {
     public void run() throws GameActionException {
         super.run();
 
-        if (currentRound == 300) {
+        if (currentRound == 400) {
             rc.resign(); // TODO: remove; just for debugging
         }
 
