d4c3bc98567561095642061aa552059ce0c70da2
Mihir <mihir.v.patel7@gmail.com>
2021-01-23 11:22:56 -0800

    going to war

---
+++ src/wbush/MapTerrain.java
@@ -0,0 +1,15 @@
+package wbush;
+
+import battlecode.common.*;
+
+public class MapTerrain {
+
+    public MapLocation loc;
+    public double pa;
+   
+    public MapTerrain(MapLocation location, double passability) {
+        this.loc = location;
+        this.pa = passability;
+    }
+    
+}
