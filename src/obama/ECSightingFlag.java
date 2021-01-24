ba147235d67f6e005a5e59b884613fe7a6826d4d
Vinjai Vale <vinjai@nightwatchapp.com>
2021-01-24 17:40:57 -0500

    new bot: obama

---
+++ src/obama/ECSightingFlag.java
@@ -0,0 +1,66 @@
+package obama;
+
+import battlecode.common.*;
+
+/**
+ * Flag that robots can use to communicate the location of a non-ally EC.
+ */
+public class ECSightingFlag extends LocationFlag {
+    /**
+     * Flag breakdown:
+     * - flag schema (SCHEMA_BITS)
+     * - mod 128 x coordinate (COORD_BITS)
+     * - mod 128 y coordinate (COORD_BITS)
+     * - type of EC that was sighted (EC_TYPE_BITS)
+     * - the approximate influence of the EC (EC_INF_BITS)
+     *
+     * Total bits used: 3 + 7 + 7 + 2 + 5 = 24.
+     */
+
+    final int EC_TYPE_BITS = 2;
+    final int EC_INF_BITS = 5;
+
+    public static final int NEUTRAL_EC = 0;
+    public static final int ENEMY_EC = 1;
+    public static final int ALLY_EC = 2;
+
+    public ECSightingFlag() {
+        super(true);
+        setSchema(EC_SIGHTING_SCHEMA);
+    }
+
+    public ECSightingFlag(MapLocation loc, int ecType, int inf) {
+        super(true);
+        setSchema(EC_SIGHTING_SCHEMA);
+        writeLocation(loc);
+        writeECType(ecType);
+        writeECInfluence(inf);
+    }
+
+    public ECSightingFlag(int received) {
+        super(received);
+    }
+
+    public boolean writeECType(int ecType) {
+        return writeToFlag(ecType, EC_TYPE_BITS);
+    }
+
+    public int readECType() {
+        return readFromFlag(SCHEMA_BITS + COORD_BITS + COORD_BITS, EC_TYPE_BITS);
+    }
+
+    public boolean writeECInfluence(int influence) {
+        int encoded = (int) ((influence - 50) / 15);
+        if (encoded > 31) encoded = 31;
+        if (encoded < 0) encoded = 0;
+        return writeToFlag(encoded, EC_INF_BITS);
+    }
+
+    /**
+     * Returns an upper bound on the EC's influence when the influence is at most 515.
+     * If it returns 530, then the influence is at least 516, but could be arbitrarily high.
+     */
+    public int readECInfluence() {
+        return 15 * readFromFlag(SCHEMA_BITS + COORD_BITS + COORD_BITS + EC_TYPE_BITS, EC_INF_BITS) + 65;
+    }
+}
---
+++ src/obama/Muckraker.java
@@ -0,0 +1,169 @@
+package obama;
+
+import battlecode.common.*;
+import java.util.*;
+
+public class Muckraker extends Unit {
+
+    public final static int INITIAL_COOLDOWN = 10;
+
+    public Muckraker(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    @Override
+    public void runUnit() throws GameActionException {
+        super.runUnit();
+
+        updateDestinationForExploration(false);
+
+        // Search for nearest slanderer. If one exists, kill it or move towards it.
+        if (rc.isReady()) {
+            if (!denyNeutralEC()) {
+                unClog();
+                huntSlanderersOrToDestination();
+            }
+        }
+        rc.setIndicatorLine(myLocation, destination, 255, 0, 0);
+    }
+
+
+    boolean unClog() throws GameActionException {
+        if (rc.canDetectLocation(destination.add(myLocation.directionTo(destination))) &&
+            rc.canDetectLocation(destination.add(myLocation.directionTo(destination).rotateLeft())) &&
+            rc.canDetectLocation(destination.add(myLocation.directionTo(destination).rotateRight()))) {
+            RobotInfo destRobot = rc.senseRobotAtLocation(destination);
+            if (destRobot != null && destRobot.team == enemyTeam && destRobot.type == RobotType.ENLIGHTENMENT_CENTER) {
+                if (myLocation.distanceSquaredTo(destination) <= 2) {
+                    return false;
+                }
+                if (   (rc.isLocationOccupied(destination.add(Direction.NORTH)) || !rc.onTheMap(destination.add(Direction.NORTH)))
+                    && (rc.isLocationOccupied(destination.add(Direction.NORTHEAST)) || !rc.onTheMap(destination.add(Direction.NORTHEAST)))
+                    && (rc.isLocationOccupied(destination.add(Direction.EAST)) || !rc.onTheMap(destination.add(Direction.EAST)))
+                    && (rc.isLocationOccupied(destination.add(Direction.SOUTHEAST)) || !rc.onTheMap(destination.add(Direction.SOUTHEAST)))
+                    && (rc.isLocationOccupied(destination.add(Direction.SOUTH)) || !rc.onTheMap(destination.add(Direction.SOUTH)))
+                    && (rc.isLocationOccupied(destination.add(Direction.SOUTHWEST)) || !rc.onTheMap(destination.add(Direction.SOUTHWEST)))
+                    && (rc.isLocationOccupied(destination.add(Direction.WEST)) || !rc.onTheMap(destination.add(Direction.WEST)))
+                    && (rc.isLocationOccupied(destination.add(Direction.NORTHWEST)) || !rc.onTheMap(destination.add(Direction.NORTHWEST)))
+                    ) {
+                        destination = new MapLocation(baseLocation.x + (int)(Math.random()*80 - 40), baseLocation.y + (int)(Math.random()*80 - 40));
+                        return true;
+                    }
+            }
+        }
+        return false;
+    }
+    /**
+     * Dilute enemy politicians when they try to capture neutral EC.
+     * Returns true if it took an action.
+     * @throws GameActionException
+     */
+    boolean denyNeutralEC() throws GameActionException {
+        if (currentRound < 100) {
+            return false;
+        }
+
+        RobotInfo[] nearbyNeutrals = rc.senseNearbyRobots(RobotType.MUCKRAKER.sensorRadiusSquared, neutralTeam);
+        // No nearby neutral units
+        if (nearbyNeutrals.length == 0) {
+            return false;
+        }
+        RobotInfo nearestNeutralEC = null;
+        int nearestNeutralECDistance = 100000;
+        for (RobotInfo robot : nearbyNeutrals) {
+            int distance = robot.location.distanceSquaredTo(myLocation);
+            if (distance < nearestNeutralECDistance) {
+                nearestNeutralECDistance = distance;
+                nearestNeutralEC = robot;
+            }
+        }
+        // No nearby neutral ECs
+        if (nearestNeutralEC == null) {
+            return false;
+        }
+        for (RobotInfo robot : nearbyAllies) {
+            // Another muckraker has EC covered
+            if (robot.type == RobotType.MUCKRAKER && robot.location.distanceSquaredTo(nearestNeutralEC.location) <= 2) {
+                return false;
+            }
+            // Ally policitian attempts to take EC
+            if (robot.type == RobotType.POLITICIAN && robot.location.distanceSquaredTo(nearestNeutralEC.location) <= 2) {
+                return false;
+            }
+        }
+        // Nearest enemy politician
+        RobotInfo nearestEnemyPolitician = null;
+        int nearestEnemyPoliticianDistance = 100000;
+        for (RobotInfo robot : nearbyEnemies) {
+            int distance = robot.location.distanceSquaredTo(nearestNeutralEC.location);
+            if (robot.type == RobotType.POLITICIAN && distance < nearestEnemyPoliticianDistance) {
+                nearestEnemyPoliticianDistance = distance;
+                nearestEnemyPolitician = robot;
+            }
+        }
+        // No enemy politicians
+        if (nearestEnemyPolitician == null) {
+            // Don't move, already adjacent
+            if (nearestNeutralECDistance > 2) {
+                fuzzyMove(nearestNeutralEC.location);
+            }
+        }
+        else {
+            // Redefine as unchanging variable for compiler
+            MapLocation nearestEnemyPoliticianLocation = nearestEnemyPolitician.location;
+            int nearestEnemyPoliticianDistanceFinal = nearestEnemyPoliticianDistance;
+            MapLocation nearestNeutralECLocation = nearestNeutralEC.location;
+            Arrays.sort(allDirections, new Comparator<Direction>() {
+                public int compare(Direction d1, Direction d2) {
+                    MapLocation m1 = myLocation.add(d1);
+                    MapLocation m2 = myLocation.add(d2);
+                    int cost1 = m1.distanceSquaredTo(nearestEnemyPoliticianLocation) <= nearestEnemyPoliticianDistanceFinal ? 0 : 100000;
+                    int cost2 = m2.distanceSquaredTo(nearestEnemyPoliticianLocation) <= nearestEnemyPoliticianDistanceFinal ? 0 : 100000;
+                    cost1 += m1.distanceSquaredTo(nearestNeutralECLocation);
+                    cost2 += m2.distanceSquaredTo(nearestNeutralECLocation);
+                    return cost1 - cost2;
+                }
+            });
+            for (Direction dir : allDirections) {
+                // Stay put
+                if (dir == Direction.CENTER) {
+                    break;
+                }
+                else if (rc.canMove(dir)) {
+                    // System.out.println("Took deny neutral EC move: " + dir);
+                    move(dir);
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Search for nearest slanderer. If one exists, kill it or move towards it.
+     * @throws GameActionException
+     */
+    void huntSlanderersOrToDestination() throws GameActionException {
+        RobotInfo nearestSlanderer = null;
+        int nearestSlandererDistSquared = 100;
+        for (RobotInfo robot : nearbyEnemies) {
+            int robotDistSquared = myLocation.distanceSquaredTo(robot.location);
+            if (robot.type == RobotType.SLANDERER && robotDistSquared < nearestSlandererDistSquared) {
+                nearestSlanderer = robot;
+                nearestSlandererDistSquared = robotDistSquared;
+            }
+        }
+        if (nearestSlanderer != null) {
+            // Nearest slanderer exists -- try to kill it or move towards it.
+            if (nearestSlandererDistSquared < rc.getType().actionRadiusSquared) {
+                rc.expose(nearestSlanderer.location);
+            } else {
+                fuzzyMove(nearestSlanderer.location);
+            }
+        } else {
+            // Continue towards destination
+            // Consider using weightedFuzzyMove
+            fuzzyMove(destination);
+        }
+    }
+}
