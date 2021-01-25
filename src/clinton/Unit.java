41b8e771ae7a0d071b8e92f90194706bcf8cd648
Mihir <mihir.v.patel7@gmail.com>
2021-01-24 20:19:07 -0800

    fix time out

--- src/clinton/Unit.java
+++ src/clinton/Unit.java
@@ -793,21 +793,22 @@ public abstract class Unit extends Robot {
                     break;
                 }
             }
-            if (edgeLocations[0] != -1 && edgeLocations[0] < destination.y
-                || edgeLocations[1] != -1 && edgeLocations[1] < destination.x
-                || edgeLocations[2] != -1 && edgeLocations[2] > destination.y
-                || edgeLocations[3] != -1 && edgeLocations[3] > destination.x) {
-                valid = false;
-            } 
+            // if (edgeLocations[0] != -1 && edgeLocations[0] < destination.y
+            //     || edgeLocations[1] != -1 && edgeLocations[1] < destination.x
+            //     || edgeLocations[2] != -1 && edgeLocations[2] > destination.y
+            //     || edgeLocations[3] != -1 && edgeLocations[3] > destination.x) {
+            //     valid = false;
+            // } 
             while (!valid) {
                 valid = true;
                 destination = new MapLocation(baseLocation.x + (int)(Math.random()*80 - 40), baseLocation.y + (int)(Math.random()*80 - 40));
-                if (edgeLocations[0] != -1 && edgeLocations[0] < destination.y
-                    || edgeLocations[1] != -1 && edgeLocations[1] < destination.x
-                    || edgeLocations[2] != -1 && edgeLocations[2] > destination.y
-                    || edgeLocations[3] != -1 && edgeLocations[3] > destination.x) {
-                    valid = false;
-                } else {
+                // if (edgeLocations[0] != -1 && edgeLocations[0] < destination.y
+                //     || edgeLocations[1] != -1 && edgeLocations[1] < destination.x
+                //     || edgeLocations[2] != -1 && edgeLocations[2] > destination.y
+                //     || edgeLocations[3] != -1 && edgeLocations[3] > destination.x) {
+                //     valid = false;
+                // } 
+                else {
                     for (int i = 0; i < priorDestinations.size(); i++) {
                         if (destination.distanceSquaredTo(priorDestinations.get(i)) < 40) {
                             valid = false;
